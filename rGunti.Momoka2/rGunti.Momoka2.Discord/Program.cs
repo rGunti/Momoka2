fe5900639ba8df137cea45f036c7ac5c4190070f
Raphael "rGunti" Guntersweiler <raphael@rgunti.ch>
2017-03-07 20:11:18 +0100

    Logging configured and implemented

--- rGunti.Momoka2/rGunti.Momoka2.Discord/Program.cs
+++ rGunti.Momoka2/rGunti.Momoka2.Discord/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace rGunti.Momoka2.Discord {
     class Program {
+        static Logger log = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args) {
+            log.Info("Hallo Welt, ich bin Momoka2 Discord Bot!");
+
+            Console.ReadLine();
         }
     }
 }
