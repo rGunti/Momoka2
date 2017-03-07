310b47342b1448f8e5628b1eb3c5ad72cbea1c09
Raphael "rGunti" Guntersweiler <raphael@rgunti.ch>
2017-03-07 19:52:26 +0100

    Project initialized

---
+++ rGunti.Momoka2/rGunti.Momoka2.Discord/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rGunti.Momoka2.Discord {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
